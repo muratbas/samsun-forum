dad0ed856e2baa6859a734ec5328eac8de24656e
muratbas <basmurat@outlook.com.tr>
2025-11-14 23:12:44 +0300

    hard reset

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com'],
-  },
-}
-
-module.exports = nextConfig
-
